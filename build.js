f4f8d6f8cb4a9e5b16090f8dbfb9335578bc9eba
falloutx <inderjitsidhu@outlook.com>
2015-09-03 09:21:25 +0530

    Add Metalsmith-stylus.

--- build.js
+++ build.js
@@ -1,11 +1,13 @@
 var metalsmith = require('metalsmith');
 var markdown = require('metalsmith-markdown');
 var templates = require('metalsmith-templates');
+var stylus = require('metalsmith-stylus');
 
 metalsmith(__dirname)
   .source('src')
   .use(markdown(require('./config/markdown')))
   .use(templates(require('./config/templates')))
+  .use(stylus(require('./config/stylus')))
   .destination('build')
   .build(function(err){
     if (err){
---
+++ config/stylus.js
@@ -0,0 +1,5 @@
+module.exports = {
+    compress: true,
+    nib: true,
+    autoprefixer: true
+};
